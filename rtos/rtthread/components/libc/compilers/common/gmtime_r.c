shixin627/SiFli-SDK

commit c385ca52d00d8036f587174bf43e89f1941dd9a8

[bug][build] Fix build error if both RT_USING_PTHREADS and RT_USING_LIBC are enabled
ext-redmine: #7184, REDMINE-7184

[Description in detail]
If _GNU_SOURCE is defined, all GNU extensions are enabled including pthread feature, which is conflicted with RT-Thread implementation.
According to rt-thread lts v3.1, POSIX version up to POSIX.1b-1993 is enabled, which means pthread is disabled in standard lib.

Change-Id: I934e93723fb0bef5a29dc177cef59ba4a195ff5c

rtos/rtthread/components/libc/compilers/common/gmtime_r.c
@@ -10,8 +10,8 @@
 #include "rtthread.h"
 #include "rthw.h"
 #include "string.h"
-#if defined(__CC_ARM) || defined(__CLANG_ARM) || defined (__IAR_SYSTEMS_ICC__)
 #include <sys/time.h>
+#if defined(__CC_ARM) || defined(__CLANG_ARM) || defined (__IAR_SYSTEMS_ICC__)
 
 /* seconds per day */
 #define SPD 24*60*60

rtos/rtthread/components/libc/compilers/newlib/SConscript
@@ -6,7 +6,8 @@ cwd = GetCurrentDir()
 group = []
 
 CPPPATH = [cwd]
-CPPDEFINES = ['RT_USING_NEWLIB']
+# only enable POSIX.1b-1993 Real-time extensions
+CPPDEFINES = ['RT_USING_NEWLIB', '_POSIX_C_SOURCE=199309L']
 
 # link with libc and libm:
 # libm is a frequently used lib. Newlib is compiled with -ffunction-sections in

tools/build/building.py
@@ -2468,7 +2468,7 @@ def SifliGccEnv(cpu):
     rtconfig.CFLAGS += ' -fno-unwind-tables -fno-exceptions'
     rtconfig.CFLAGS += ' -fno-common'
     
-    rtconfig.CFLAGS += ' -Os -D_GNU_SOURCE'
+    rtconfig.CFLAGS += ' -Os'
     rtconfig.CXXFLAGS = rtconfig.CFLAGS
     if no_dsp_fp:
         rtconfig.CXXFLAGS += ' -fno-exceptions -fno-rtti'

commit ca1ca4d267d0e2e84e71674f2e5a47cb87306ca9

[bug][build] Fix simulator build introduced by c385ca52d
ext-redmine: #7184, REDMINE-7184

[Description in detail]

Change-Id: I1c9e65d15a844e693ad9cb6f9199054fe4587fa8

rtos/rtthread/components/libc/compilers/common/gmtime_r.c
@@ -10,7 +10,10 @@
 #include "rtthread.h"
 #include "rthw.h"
 #include "string.h"
-#include <sys/time.h>
+#if defined(__CC_ARM) || defined(__CLANG_ARM) || defined (__IAR_SYSTEMS_ICC__) || defined(__GNUC__)
+    #include <sys/time.h>
+#endif /* __CC_ARM || __CLANG_ARM || __IAR_SYSTEMS_ICC__ || __GNUC__ */
+
 #if defined(__CC_ARM) || defined(__CLANG_ARM) || defined (__IAR_SYSTEMS_ICC__)
 
 /* seconds per day */