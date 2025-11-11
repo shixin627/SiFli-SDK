eec7f84c36160f261f3ef30db3cfd1f0bb2b40e6
ybzhao <ybzhao@sifli.com>
2025-11-11 19:56:13 +0800

    [bug][ble] fix hid example array out-of-bounds access
    redmine: #8631, REDMINE-8631
    
    ext-redmine: #id, REDMINE-id
    
    [Description in detail]
    
    Change-Id: I25a8d25c3633f42042ba5923b5edc4be82cf7c4e
    (cherry picked from commit cff10383b5581ba1e7ec8a793e8fe235ace0c89a)

--- example/ble/hid/src/main.c
+++ example/ble/hid/src/main.c
@@ -460,7 +460,7 @@ static int hid_kbd_state_key_set(uint8_t key)
         hid_keyboard_state.ctrl_keys_state |= ctrl_mask;
         return 0;
     }
-    for (size_t i = 0; i < KEY_CTRL_CODE_MAX; ++i)
+    for (size_t i = 0; i < KEY_PRESS_MAX; ++i)
     {
         if (hid_keyboard_state.keys_state[i] == 0)
         {
@@ -481,7 +481,7 @@ static int hid_kbd_state_key_clear(uint8_t key)
         hid_keyboard_state.ctrl_keys_state &= ~ctrl_mask;
         return 0;
     }
-    for (size_t i = 0; i < KEY_CTRL_CODE_MAX; ++i)
+    for (size_t i = 0; i < KEY_PRESS_MAX; ++i)
     {
         if (hid_keyboard_state.keys_state[i] == key)
         {
