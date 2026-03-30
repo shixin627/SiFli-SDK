e1fa0fb0a4d6fb94fc8d8515b1cc935c552457d4
TSENG-YU-FENG <157343631+TSENG-YU-FENG@users.noreply.github.com>
2026-03-30 10:04:13 +0800

    feat:save_emoji

--- example/get-started/dualcore/src/hcpu/gui_apps/common/ui_img_helper.h
+++ example/get-started/dualcore/src/hcpu/gui_apps/common/ui_img_helper.h
@@ -239,7 +239,9 @@ LV_IMG_DECLARE(notification_img);
 #define ICON_X ((const void *)&icon_x)
 #define CALCULATOR_ICON ((const void *)&calculator_icon)
 #define FIND_PHONE ((const void *)&find_phone)
-#define FLISHLIGHT_ICON ((const void *)&flashlight_icon)
+// #define FLISHLIGHT_ICON ((const void *)&flashlight_icon)
+#define FLISHLIGHT_ICON "/assets/icons/flashlight_icon.bin"
+
 #define IMG_MAIL ((const void *)&img_mail)
 #define NOTIFICATION_IMG ((const void *)&notification_img)
 // #define GAUS_CLOCK5_BG (&gaus_clock5_bg)
--- middleware/lvgl/lvsf/lvsf_emoji.c
+++ middleware/lvgl/lvsf/lvsf_emoji.c
@@ -6,53 +6,37 @@
 
 #include "rtthread.h"
 #include "lv_ext_resource_manager.h"
-#ifdef RT_USING_DFS
-    #include <dfs.h>
-    #include <dfs_file.h>
-#endif
 
 #if defined (EMOJI_SUPPORT)
-    #include "images_emoji_header.h"
-#endif
 
-#if 0 //ifdef RT_USING_DFS
-#define EMOJI_RES_PATH    "/ex/resource/"
+#define EMOJI_RES_PATH    "/assets/emoji/"
 #define EMOJI_RES_SUFFIX  ".bin"
 
-//emoji filename format:  emoji_xxx. where xxx is letter's unicode.
-char emoji_file_name[60];
+static char emoji_file_name[60];
 
-static inline void *lv_get_emoji_by_unicode(uint32_t u_letter)
-{
-    RT_ASSERT(strlen(EMOJI_RES_PATH EMOJI_RES_SUFFIX "emoji_") + 8 < 60);
-    sprintf(emoji_file_name, "%semoji_%x%s", EMOJI_RES_PATH, u_letter, EMOJI_RES_SUFFIX);
-    //sprintf(emoji_file_name, "%s%s%s", "/ex/resource/", "clock_bg", ".bin");
-
-    struct stat buf;
-    if (RT_EOK == dfs_file_stat(emoji_file_name, &buf))
-    {
-        return &emoji_file_name;
-    }
-
-    return NULL;
-}
-#else
 void *lv_get_emoji_by_unicode(uint32_t u_letter)
 {
 #define GET_EMOJI_INFO(_id) \
     case 0x##_id: \
     { \
-    return (void *)&emoji_##_id;\
+        sprintf(emoji_file_name, "%semoji_%s%s", EMOJI_RES_PATH, #_id, EMOJI_RES_SUFFIX); \
+        return emoji_file_name; \
     }
 
     switch (u_letter)
     {
-#ifdef EMOJI_SUPPORT
 #include "emoji_info.h"
-#endif
     default:
         return NULL;
     }
 }
+
+#else
+
+void *lv_get_emoji_by_unicode(uint32_t u_letter)
+{
+    return NULL;
+}
+
 #endif
 
