fcce476c223213fbb62c83060e51df53abb63559
Azhi <48245812+shixin627@users.noreply.github.com>
2026-05-27 19:56:07 +0800

    feat: increase CONFIG_FT_CACHE_SIZE

--- customer/boards/eh-lb56xu/bsp_board.h
+++ customer/boards/eh-lb56xu/bsp_board.h
@@ -99,7 +99,7 @@ extern int __bss_end;
 #define BOARD_VER_28 28
 #define BOARD_VER_29 29
 
-#define CUSTOMER_BOARD_VER BOARD_VER_28
+#define CUSTOMER_BOARD_VER BOARD_VER_29
 
 #define ENABLE_TAP_AND_HOLD 0
 #define MAX_RAWDATA_TIME_STEP 35
